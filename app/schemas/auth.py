909f77d036c723735534e300ae1b98b4674b0250
Constantin Ilie <intern.constantin.ilie@assist.ro>
2026-08-05 11:33:59 +0300

    feat: RefreshToken request+response model

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -19,3 +19,11 @@ class LoginResponse(BaseModel):
     access_token: str
     refresh_token: str
     token_type: str = "bearer"
+
+class RefreshTokenRequest(BaseModel):
+    refresh_token: str
+
+class RefreshTokenResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
