c8ceb106cd8e03445c2ae714ebe09df6ffe2b925
Constantin Ilie <intern.constantin.ilie@assist.ro>
2026-08-17 14:24:06 +0300

    fix: changed URL to keep consistency across services

--- app/infrastructure/core.py
+++ app/infrastructure/core.py
@@ -10,7 +10,7 @@ async def sync_user_to_core(user_id: str, email: str, role: str)->None:
         "role":role,
     }
     headers ={"X-Service-Key": settings.INTERNAL_API_KEY}
-    response = await http_state.http_client.post(settings.CORE_SERVICE_URL, json=payload, headers=headers)
+    response = await http_state.http_client.post(f'{settings.CORE_SERVICE_URL}/api/users/sync/', json=payload, headers=headers)
     if response.status_code != 201:
         raise CoreServiceException()
 
