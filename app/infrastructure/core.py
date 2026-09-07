fdfc4621ba3bf25b69c5c449fb72a619aa7486a1
Constantin Ilie <intern.constantin.ilie@assist.ro>
2026-09-07 10:45:37 +0300

    hotfix: removed error 200 too from hard deleting user

--- app/infrastructure/core.py
+++ app/infrastructure/core.py
@@ -11,6 +11,6 @@ async def sync_user_to_core(user_id: str, email: str, role: str)->None:
     }
     headers ={"X-Service-Key": settings.INTERNAL_API_KEY}
     response = await http_state.http_client.post(f'{settings.CORE_SERVICE_URL}/api/users/sync/', json=payload, headers=headers)
-    if response.status_code != 201:
+    if response.status_code not in (200, 201):
         raise CoreServiceException()
 
